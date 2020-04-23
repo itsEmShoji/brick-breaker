4c7cf49449f34e461e54a8bb9aa27d2cb3d7ef60
Emily Shoji <eshoji1@lion.lmu.edu>
2020-04-23 12:48:25 -0700

    add interactivity

--- practice.py
+++ practice.py
@@ -3,9 +3,11 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 done = False
+is_blue = True
+x = 30
+y = 30
 
 while not done:
-    is_blue = True
 
     # check if we hit the close window button
     for event in pygame.event.get():
@@ -14,11 +16,21 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= 3
+    if pressed[pygame.K_DOWN]:
+        y += 3
+    if pressed[pygame.K_LEFT]:
+        x -= 3
+    if pressed[pygame.K_RIGHT]:
+        x += 3
+
     if is_blue:
         color = (0, 128, 255)
     else:
         color = (225, 100, 0)
 
-    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
 
     pygame.display.flip()
