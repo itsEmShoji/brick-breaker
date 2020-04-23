60dfd73529d6d2fdf2415a97e9534a8cbd76c18d
Emily Shoji <eshoji1@lion.lmu.edu>
2020-04-23 12:26:18 -0700

    Add rectangle
    Co-authored-by: Bhwin3 <bwin29@claytonschools.net>

--- practice.py
+++ practice.py
@@ -5,8 +5,12 @@ screen = pygame.display.set_mode((400, 300))
 done = False
 
 while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-        
-        pygame.display.flip()
+
+    # check if we hit the close window button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+
+    pygame.display.flip()
